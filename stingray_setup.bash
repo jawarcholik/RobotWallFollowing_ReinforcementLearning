b5ec99118e22ba6edea10f88eaa48307a177776a
erbornemeier <erbornemeier@mines.edu>
2020-02-03 20:05:42 -0700

    moved env variables to seperate bash script since setup.bash regenerates on catkin_make

---
+++ stingray_setup.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export GAZEBO_RESOURCE_PATH=~/Stingray-Simulation/catkin_ws/src/stingray_sim
+export GAZEBO_MODEL_PATH=~/Stingray-Simulation/catkin_ws/src/stingray_sim/models
+export GAZEBO_PLUGIN_PATH=~/Stingray-Simulation/catkin_ws/src/stingray_sim/plugins/build
+
