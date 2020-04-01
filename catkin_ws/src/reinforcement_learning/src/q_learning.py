70825192a4e60991a7b555e759ec1be73d3d12f3
Jacob <jawarcholik@mymail.mines.edu>
2020-04-01 12:17:43 -0600

    Changed the isStuck Conditions to use numpy.close(). Seems to fix buffering LaserScan messages between episodes.

--- catkin_ws/src/reinforcement_learning/src/q_learning.py
+++ catkin_ws/src/reinforcement_learning/src/q_learning.py
@@ -5,6 +5,9 @@ import rospy
 from std_srvs.srv import Empty
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Pose2D
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.srv import GetModelState
 import math
 import numpy as np
 import random
@@ -27,9 +30,9 @@ State Space
 Action spaces
 """
 ACTIONS = {
-    'F': [.3,0],
-    'L': [.3,math.pi/4],
-    'R': [.3,-math.pi/4],
+    'F': [.2,0],
+    'L': [.2,math.pi/4],
+    'R': [.2,-math.pi/4],
     'S': [0,0]
 }
 
@@ -57,7 +60,8 @@ class qTable():
         leftWeight = 0
         rightWeight = 0
         tup = tuple(state)
-        if tup  not in self.table:
+
+        if tup not in self.table:
             #### Naeve Implementation ####
             # if state[1] != 'TC':
             #     forwardWeight += 10
@@ -80,20 +84,25 @@ class qTable():
 
             ### Less Naeve ###
             # if state[1] not in ['TC', 'C'] and state[3] not in ['TF','TC']:
-            #     forwardWeight += 10
+            #     forwardWeight += 5
             #     if state[3] == 'M':
             #         forwardWeight += 5
             # if state[0] == 'C':
-            #     leftWeight -= 5
+            #     leftWeight -= 1
+            #     rightWeight += 1
             # if state[2] == 'C':
-            #     leftWeight += 5
-            #     rightWeight -= 5
+            #     leftWeight += 3
+            #     rightWeight -= 3
             #     forwardWeight -= 2
             # if state[3] in ['F', 'TF']:
-            #     # rightWeight += 5
-            #     if state[3] == 'TF':
-            #         leftWeight -= 5
+            #     rightWeight += 1
+            #     # if state[3] == 'TF':
+            #         # leftWeight -= 2
+            # if state[4] in ['U']:
+            #     forwardWeight += 3
+            #     leftWeight += 1
             ##################
+
             self.table[tup] = [forwardWeight, leftWeight, rightWeight]
             self.initialTable[tup] = [forwardWeight, leftWeight, rightWeight]
 
@@ -123,18 +132,24 @@ class wallFollowEnv():
         self.actionSpace = ACTIONS
         self.possibleActions = ['F','L','R','S']
 
-        self.previousStuckFront = 0
-        self.previousStuckLeft = 0
-        self.previousStuckRight = 0
+        self.previousStucks = 0
+        # self.previousStuckFront = 0
+        # self.previousStuckLeft = 0
+        # self.previousStuckRight = 0
 
         # Store memory of the agents actions
         # These will be the state change the agent made during one runthrough of the algorithm
         self.action_episode_memory = []
-        # self.initial_q_table = dict()
-        # self.final_q_table = dict()
         self.state_episode_memory = []
+        self.previousEpisodeScans = []
+        self.currentEpisodeScans = []
 
-    def _step_env(self, action=0):
+        rospy.wait_for_service('/gazebo/get_model_state')
+        self.getPosition = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+
+        self.previousPosition = ModelState()
+
+    def _step_env(self, action='S'):
         self.robot.step(action)
         self.current_step += 1
         observation = self._get_observation()
@@ -145,6 +160,7 @@ class wallFollowEnv():
         #Test info
         self.action_episode_memory.append(action)
         self.state_episode_memory.append(observation)
+        self.currentEpisodeScans.append(self.robot.ranges)
 
         return observation, reward, done, info
         # return observation
@@ -160,9 +176,15 @@ class wallFollowEnv():
     def _isDone(self, obs):
         if self.isStuck(obs):
             print("Stuck")
-            if self.current_step < 10:
-                print(obs)
-                print(self.previousStucks)
+            # if self.current_step < 10:
+            #     print(obs)
+            #     print('Front Stuck: ', self.previousStuckFront)
+            #     print('Right Stuck: ', self.previousStuckRight)
+            #     with open('/home/jawarcholik/Stingray-Simulation/catkin_ws/src/reinforcement_learning/src/previousScans.txt', 'w') as fp:
+            #         json.dump(self.previousEpisodeScans, fp)
+            #     with open('/home/jawarcholik/Stingray-Simulation/catkin_ws/src/reinforcement_learning/src/currentScans.txt', 'w') as fp:
+            #         json.dump(self.currentEpisodeScans, fp)
+            #     exit()
             return True
         if self.current_step >= 10000:
             print("Full Episode")
@@ -180,47 +202,50 @@ class wallFollowEnv():
         else:
             self.good_policy_step = 0
 
-        # Is the Robot Stuck Front
-        if obs[1] == 'TC':
-            # print("Stuck")
-            self.previousStuckFront+=1
-            if self.previousStuckFront > 3:
-                # print(self.robot.ranges)
-                return True
-        if obs[1] != 'TC':
-            # if self.previousStucks != 0:
-                # print("Unstuck")
-            self.previousStuckFront = 0
+        currentPosition = self.getPosition('triton_lidar','world')
 
-        # Is the Robot Stuck Left
-        if obs[0] == 'C':
-            self.previousStuckLeft+=1
-            if self.previousStuckLeft > 3:
-                return True
-        if obs[0] != 'C':
-            self.previousStuckLeft = 0
+        if np.isclose(currentPosition.pose.position.x, self.previousPosition.pose.position.x, atol=.01) and np.isclose(currentPosition.pose.position.y, self.previousPosition.pose.position.y, atol=.01):
+            self.previousStucks += 1
+        else:
+            self.previousStucks = 0
 
-        # Is the Robot Stuck Right
-        if obs[3] == 'TC':
-            self.previousStuckRight+=1
-            if self.previousStuckRight > 3:
-                return True
-        if obs[3] != 'TC':
-            self.previousStuckRight = 0
+        if self.previousStucks > 3:
+            return True
 
+        self.previousPosition = currentPosition
 
         return False
 
     def reset(self):
+        #Reset the World
         rospy.wait_for_service('/gazebo/reset_world')
         resetSim = rospy.ServiceProxy('/gazebo/reset_world', Empty)
         resetSim()
+
+        #Delete the Robot Instance in Attempt to Clear Buffer
+        del self.robot
+        self.robot = tritonRobot()
+
+        #Choose a random Spawn
+        rospy.wait_for_service('/gazebo/set_model_state')
+        setPosition = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+
+        spawn = ModelState()
+        spawn.model_name = 'triton_lidar'
+        spawn.pose.position.x = random.randint(-4,3) + .5
+        spawn.pose.position.y = random.randint(-4,3) + .5
+        setPosition(spawn)
+
         self.current_step = -1
-        self.previousStucks = 0
+        self.previousStuckFront = 0
+        self.previousStuckRight = 0
+        # self.previousStuckLeft = 0
         self.good_policy_step = 0
         self.current_episode += 1
+        self.previousEpisodeScans = self.currentEpisodeScans
+        self.currentEpisodeScans = []
         time.sleep(2)
-        obs = self._get_observation() # This could be an issue should this be an enviornment step?
+        obs = self._step_env()[0] #CHANGED: This could be an issue should this be an enviornment step?
         return obs
 
 """
@@ -230,18 +255,27 @@ class tritonRobot:
     def __init__(self):
         rospy.init_node('triton')
         self.pub = rospy.Publisher('/triton_lidar/vel_cmd', Pose2D, queue_size=2)
-        self.sub = rospy.Subscriber('/scan', LaserScan, self.callback)
+        # self.sub = rospy.Subscriber('/scan', LaserScan, self.callback)
+        # self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        # self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
         self.scan = LaserScan()
 
-        self.rate = rospy.Rate(5)
+        self.rate = rospy.Rate(10)
         self.ranges = []
-        self.state = ['','','','']
-        self.discreteDistances = ['TC','C','M','F','TF']
+        self.previousRanges = []
+        self.state = ['','','','','']
+        self.previousState = ['','','','','']
+        self.discreteDistances = ['TTC','TC','C','M','F','TF']
+        self.discreteOrientations = ['U','P','A','MA'] # Undefined, Parallel, Approaching, Moving Away
 
     def callback(self, msg):
+        # rospy.wait_for_service('/gazebo/pause_physics')
+        # try:
+        #     self.pause()
+        # except (rospy.ServiceException) as e:
+        #     print ("/gazebo/pause_physics service call failed")
         # print("Entering Callback")
         self.ranges = msg.ranges
-        # self.sub.unregister()
 
         #Interpret the LaserScan ranges
         front = 9999
@@ -250,94 +284,111 @@ class tritonRobot:
         left = 9999
 
         #Getting left min distance
-        # for i in range(150,210):
-        #     if self.ranges[i] < left:
-        #         left = self.ranges[i]
-        for i in range(30,91):
+        for i in range(60,121):
             if self.ranges[i] < left:
                 left = self.ranges[i]
 
         #Determining Discrete Distance Measure: Close, Medium, Far, etc.
-        if left <= .5:
-            self.state[0] = self.discreteDistances[1]
+        if left <= .25:
+            self.state[0] = self.discreteDistances[0]
+        elif left <= .5:
+            self.state[0] = self.discreteDistances[2]
         elif left > .5:
-            self.state[0] = self.discreteDistances[3]
+            self.state[0] = self.discreteDistances[4]
         else:
             print("Error with left values")
 
 
         #Getting front min distance
-        # for i in range(60,121):
-        #     if self.ranges[i] < front:
-        #         front = self.ranges[i]
         for i in range(-30,31):
             if self.ranges[i] < front:
                 front = self.ranges[i]
 
         #Determining Discrete Distance Measure: Close, Medium, Far, etc.
-        if front < .5:
+        if front <= .25:
             self.state[1] = self.discreteDistances[0]
-        elif front < .6:
+        elif front < .5:
             self.state[1] = self.discreteDistances[1]
-        elif front <= 1.2:
+        elif front < .6:
             self.state[1] = self.discreteDistances[2]
-        elif front > 1.2:
+        elif front <= 1.2:
             self.state[1] = self.discreteDistances[3]
+        elif front > 1.2:
+            self.state[1] = self.discreteDistances[4]
         else:
             print("Error with front values")
 
 
         #Getting right min distance
-        # for i in range(30):
-        #     if self.ranges[i] < right:
-        #         right = self.ranges[i]
-        #
-        # for i in range(329,359):
-        #     if self.ranges[i] < right:
-        #         right = self.ranges[i]
-        for i in range(-90,-29):
+        for i in range(-120,-59):
             if self.ranges[i] < right:
                 right = self.ranges[i]
 
         #Determining Discrete Distance Measure: Close, Medium, Far, etc.
-        if right < .5:
+        if right <= .25:
             self.state[3] = self.discreteDistances[0]
-        elif right < .6:
+        elif right < .5:
             self.state[3] = self.discreteDistances[1]
-        elif right <= .8:
+        elif right < .6:
             self.state[3] = self.discreteDistances[2]
-        elif right <= 1.2:
+        elif right <= .8:
             self.state[3] = self.discreteDistances[3]
+        elif right <= 1.2:
+            self.state[3] = self.discreteDistances[4]
         elif right > 1.2:
-            self.state[3] = self.discreteDistances[4] #####FIXME Took out TF  [4]
+            self.state[3] = self.discreteDistances[5] #####FIXME Took out TF  [4]
         else:
             print("Error with right values")
 
 
         #Getting rightFront min distance
-        # for i in range(121,150):
-        #     if self.ranges[i] < rightFront:
-        #         rightFront = self.ranges[i]
         for i in range(-60,-29):
             if self.ranges[i] < rightFront:
                 rightFront = self.ranges[i]
 
         #Determining Discrete Distance Measure: Close, Medium, Far, etc.
-        if rightFront <= 1.2:
-            self.state[2] = self.discreteDistances[1]
+        if rightFront <= .25:
+            self.state[2] = self.discreteDistances[0]
+        elif rightFront <= 1.2:
+            self.state[2] = self.discreteDistances[2]
         elif rightFront > 1.2:
-            self.state[2] = self.discreteDistances[3]
+            self.state[2] = self.discreteDistances[4]
         else:
             print("Error with rightFront values")
+
+        #Determine the Orientation
+        if len(self.previousRanges) == 0 or (self.state[3] == 'TF' and self.previousState[3] == 'TF'):
+            self.state[4] = self.discreteOrientations[0]
+        elif self.state[3] == self.previousState[3]:
+            self.state[4] = self.discreteOrientations[1]
+        else:
+            if self.state[3] in ['TF','F'] and self.previousState[3] in ['F','M','C']:
+                self.state[4] = self.discreteOrientations[3]
+            elif self.state[3] in ['C','TC'] and self.previousState[3] in ['F','M','C']:
+                self.state[4] = self.discreteOrientations[2]
+
+
+        self.previousRanges = self.ranges
+        self.previousState = self.state
 
     def get_observation(self):
         # print("Robot Observation")
         self.sub = rospy.Subscriber('/scan', LaserScan, self.callback)
+        # self.scan = rospy.wait_for_message('/scan', LaserScan, timeout=10)
+
+
         # self.rate.sleep()
         # print("Exiting Observation")
         return self.state
 
-    def step(self, action=0):
+    def step(self, action='S'):
+        # rospy.wait_for_service('/gazebo/unpause_physics')
+        # try:
+        #     self.unpause()
+        # except (rospy.ServiceException) as e:
+        #     print ("/gazebo/unpause_physics service call failed")
+
+
         geoValues = ACTIONS[action]
 
         msg = Pose2D()
@@ -352,6 +403,7 @@ class tritonRobot:
             self.rate.sleep()
         except rospy.ROSTimeMovedBackwardsException as e:
             # pass
+            rate._reset
             backwardsException += 1
             # print("Time Backwards")
 
@@ -366,27 +418,38 @@ class tritonRobot:
         front = self.state[1]
         rightFront = self.state[2]
         right = self.state[3]
+        orientation = self.state[4]
 
         reward = 0
 
         # Basic reward Function
-        if right in ['TF', 'TC']:
-            reward -= 1
-        if left == 'C':
-            reward -= 5
-        if right in ['F', 'C'] or left == 'F':
-            reward += 5
-        if front == 'TC':
-            reward -= 25
-        if front == 'C':
-            reward -= 10
-        if right == 'M' and front != 'TC':
-            reward += 20
-        if rightFront == 'C':
-            reward -= 2
+        # if right in ['TF', 'TC']:
+        #     reward -= 1
+        # # if left == 'C':
+        # #     reward -= 2
+        # if right in ['F', 'C']:
+        #     reward += 2
+        # if front == 'TC':
+        #     reward -= 7
+        # if front == 'C':
+        #     reward -= 3
+        # if right == 'M' and front != 'TC':
+        #     reward += 10
+        # if rightFront == 'C':
+        #     reward -= 2
+        # if right == 'M' and orientation == 'P' and front != 'TC':
+        #     reward += 15
+        #######################
         # elif right == 'M':
             # reward += 5
 
+        #### New Attempt ####
+        if left == 'C' or front == 'TC' or right in ['TC','TF']:
+            reward = -1
+        if right in ['C','M','F'] and front != 'TC':
+            reward = 1
+        if right in ['C','M','F'] and front != 'TC':
+            reward += .5
 
         return reward
 
@@ -441,7 +504,7 @@ if __name__ == '__main__':
             GAMMA = .8
             DECAY = .985
 
-            numGames = 40
+            numGames = 150
             totalRewards = np.zeros(numGames)
 
             for i in range(numGames):
@@ -475,8 +538,16 @@ if __name__ == '__main__':
 
                     action_, actionIndex_ = qTable.maxAction(observation_, env.possibleActions)
 
+
+
                     #Update Function
-                    qTable.table[tuple(observation)][actionIndex] = qTable.table[tuple(observation)][actionIndex] + ALPHA*(reward + GAMMA*qTable.table[tuple(observation_)][actionIndex_] - qTable.table[tuple(observation)][actionIndex])
+                    oldValue = qTable.table[tuple(observation)][actionIndex]
+                    maxValue = qTable.table[tuple(observation_)][actionIndex_]
+
+                    # print(type(oldValue))
+                    # print(type(maxValue))
+
+                    qTable.table[tuple(observation)][actionIndex] =  oldValue + ALPHA*(reward + GAMMA*maxValue - oldValue)
 
                     observation = observation_
                 # print("Episode Steps: " + str(epSteps))
@@ -546,3 +617,40 @@ ob, reward, episode_over, info : tuple
          However, official evaluations of your agent are not allowed to
          use this for learning.
 """
+# def isStuck(self, obs):
+#     # Is the Step Good
+#     if obs[1] not in ['TC','C'] and obs[3] == 'M':
+#         self.good_policy_step += 1
+#     else:
+#         self.good_policy_step = 0
+#
+#     # Is the Robot Stuck Front
+#     if obs[1] == 'TTC':
+#         # print("Stuck")
+#         self.previousStuckFront+=1
+#         if self.previousStuckFront > 7:
+#             # print(self.robot.ranges)
+#             return True
+#     if obs[1] != 'TTC':
+#         # if self.previousStucks != 0:
+#             # print("Unstuck")
+#         self.previousStuckFront = 0
+#
+#     # Is the Robot Stuck Left
+#     # if obs[0] == 'C':
+#     #     self.previousStuckLeft+=1
+#     #     if self.previousStuckLeft > 3:
+#     #         return True
+#     # if obs[0] != 'C':
+#     #     self.previousStuckLeft = 0
+#
+#     # Is the Robot Stuck Right
+#     if obs[3] == 'TTC':
+#         self.previousStuckRight+=1
+#         if self.previousStuckRight > 7:
+#             return True
+#     if obs[3] != 'TTC':
+#         self.previousStuckRight = 0
+#
+#
+#     return False
